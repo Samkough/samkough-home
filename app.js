c51b87da071aff1d902777a0e6d2cd67f9ed0ec2
Sammy Samkough <sammysamkough@gmail.com>
2019-05-02 17:15:52 -0400

    Added xss stuff so we can access backend data; probably won't need it

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const express = require("express")
 const morgan = require("morgan")
 const mysql = require("mysql")
-const bodyParser = require("body-parser");
+const bodyParser = require("body-parser")
 
 const app = express()
 const userRouter = require('./routes/user.js')
 const PORT = process.env.PORT || 5000;
 
 let allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', "*");
-    res.header('Access-Control-Allow-Headers', "*");
+    res.header('Access-Control-Allow-Origin', "*")
+    res.header('Access-Control-Allow-Headers', "*")
     next();
   }
   app.use(allowCrossDomain);
